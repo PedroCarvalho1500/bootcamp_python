559575bafaca13f6b2d74207140b8919e81b71f5
PedroCarvalho1500 <pedro_alves@gec.inatel.br>
2023-04-02 22:53:17 -0300

    ADD: flashcardapp continuing.

--- flashcard_app/main.py
+++ flashcard_app/main.py
@@ -1,28 +1,44 @@
+import time
 from tkinter import *
 import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+timer = None
+current_french_word = ""
 
+time_passed = 0
+data_words = pandas.read_csv("data/french_words.csv")
+to_learn = data_words.to_dict(orient="records")
 
-def generate_random_french_word(dictionary):
-    french, english = random.choice(list(dictionary.items()))
-
-    print(french, english)
-
-    return french
+def generate_random_french_word():
+    global current_french_word
+    current_french_word = random.choice(to_learn)
+    return current_french_word["French"]
     
 
-def generate_random_english_word(dictionary):
-    french, english = random.choice(list(dictionary.items()))
+def get_the_english_word():
+    global current_french_word
+    return current_french_word["English"]
 
-    print(french, english)
 
-    return english
+
 
 
 def update_front_card(canvas):
-    canvas.itemconfig(canvas_front_text, text=generate_random_french_word(words_dict))
+    canvas.itemconfig(canvas_text, text=generate_random_french_word())
+
+
+def change_to_back_card(canvas, window, new_image):
+    canvas.itemconfig(image_front, image=new_image)
+    canvas.itemconfig(canvas_text, text=get_the_english_word())
+    canvas.itemconfig(canvas_language, text="English",font=("Arial", 40, 'italic'))
+    window.after_cancel(window)
+
+    
+    
+
+
 
 if __name__ == '__main__':
     window = Tk()
@@ -30,28 +46,36 @@ if __name__ == '__main__':
     window.config(padx=100, pady=100, background=BACKGROUND_COLOR, height=528, width=800)
     window.resizable(False,False)
 
-    data_words = pandas.read_csv("data/french_words.csv")
 
+    #print(to_learn)
+    #data_frame = pandas.DataFrame.to_dict(data_words, orient="records")
+
+
+    #print(data_frame)
     words_dict = {row.French: row.English for (index, row) in data_words.iterrows()}
-    #print(words_dict)
+    
+    
+    
 
+    flashcard_canvas = Canvas(width=800, height=518, background=BACKGROUND_COLOR, highlightthickness=0)
+    my_flashcard_image = PhotoImage(file="images/card_front.png")
+    new_image = PhotoImage(file="images/card_back.png")
 
-    flashcard_canvas_front = Canvas(width=800, height=518, background=BACKGROUND_COLOR, highlightthickness=0)
-    my_flashcard_front_image = PhotoImage(file="images/card_front.png")
-    flashcard_canvas_front.create_image(400,270,image=my_flashcard_front_image)
-    canvas_front_text = flashcard_canvas_front.create_text(400,263,text=generate_random_french_word(words_dict),font=("Arial", 60, 'bold'))
-    canvas_front_language = flashcard_canvas_front.create_text(400,150,text="French",font=("Arial", 40, 'italic'))
-    flashcard_canvas_front.grid(column=1, row=1, columnspan=2)
+    image_front = flashcard_canvas.create_image(400,270,image=my_flashcard_image)
+    canvas_text = flashcard_canvas.create_text(400,263,text=generate_random_french_word(),font=("Arial", 60, 'bold'))
+    canvas_language = flashcard_canvas.create_text(400,150,text="French",font=("Arial", 40, 'italic'))
+    flashcard_canvas.grid(column=1, row=1, columnspan=2)
 
 
 
     right_image = PhotoImage(file="images/right.png")
-    button_right = Button(image=right_image, highlightthickness=0, command=lambda: update_front_card(flashcard_canvas_front))
+    button_right = Button(image=right_image, highlightthickness=0, command=lambda: update_front_card(flashcard_canvas))
     button_right.grid(column=2, row=2, pady=50, padx=50)
 
 
     left_image = PhotoImage(file="images/wrong.png")
-    button_left = Button(image=left_image, highlightthickness=0, command=lambda: update_front_card(flashcard_canvas_front))
+    button_left = Button(image=left_image, highlightthickness=0, command=lambda: update_front_card(flashcard_canvas))
     button_left.grid(column=1, row=2, pady=50, padx=50)
 
+    window.after(3000, change_to_back_card, flashcard_canvas, window, new_image)
     window.mainloop()
